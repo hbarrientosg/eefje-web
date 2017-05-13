a2cdc7e4852eaf642b9af08a917d54c0839a33e6
Humberto Barrientos <Hawksmoor@gmail.com>
2017-05-14 00:48:48 +0200

    repair is disabled

--- wp-config.php
+++ wp-config.php
@@ -39,7 +39,7 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('WP_ALLOW_REPAIR', true);
+define('WP_ALLOW_REPAIR', false);
 /**#@+
  * Authentication Unique Keys and Salts.
  *
