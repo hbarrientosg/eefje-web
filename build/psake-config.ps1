3b0c4a0b20c925099ac3be3a4023db2ef0fa2631
Humberto Barrientos <Hawksmoor@gmail.com>
2017-09-06 23:10:21 +0200

    delete everything

--- build/psake-config.ps1
+++
@@ -1,5 +0,0 @@
-﻿$config.buildFileName="default.ps1"
-$config.taskNameFormat="Executing {0}"
-$config.verboseError= $false
-$config.coloredOutput = $true
-$config.taskNameFormat= { param($taskName) "Executing $taskName at $(get-date)" }
--- build/psake.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-case "$1" in
-   -h)
-     powershell -NoProfile -ExecutionPolicy Bypass -Command "$(dirname $0)\psake.ps1 -help"
-     ;;
-   *)
-   powershell -NoProfile -ExecutionPolicy Bypass -Command "& $(dirname $0)\psake.ps1 $*; if (\$psake.build_success -eq \$false) { exit 1 } else { exit 0 }"
-esac
